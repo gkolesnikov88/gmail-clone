49b3406d5fe4f1a2d63eb186c1d94e1c6684555b
George Kolesnikov <gkolesnikov88@gmail.com>
2022-02-05 11:16:48 +0300

    Added router.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 import './App.css';
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Sidebar/>
-    </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="app__body">
+            <Sidebar/>
+            <Routes>
+                <Route path="/mail">
+                    {/*<Mail />*/}
+                </Route>
+                <Route path="/">
+                    {/*<EmailList />*/}
+                </Route>
+            </Routes>
+          </div>
+        </div>
+      </Router>
   );
 }
 
