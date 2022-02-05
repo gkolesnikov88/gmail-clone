28a7bf94aae9efbb943155c33de6044de7f89e90
George Kolesnikov <gkolesnikov88@gmail.com>
2022-02-05 10:56:23 +0300

    Added Sidebar option buttons.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './Sidebar.css';
-import { Button } from "@mui/material";
-import { Add } from "@mui/icons-material";
+import {Button, IconButton} from "@mui/material";
+import {
+    AccessTime,
+    Add,
+    Duo,
+    ExpandMore,
+    Inbox,
+    LabelImportant,
+    NearMe,
+    Note,
+    Person,
+    Phone,
+    Star
+} from "@mui/icons-material";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar(props) {
     return (
@@ -12,6 +25,42 @@ function Sidebar(props) {
             >
                 Compose
             </Button>
+
+            <SidebarOption
+                Icon={Inbox} title={'Inbox'} number={54} selected={true}
+            />
+            <SidebarOption
+                Icon={Star} title={'Starred'} number={54}
+            />
+            <SidebarOption
+                Icon={AccessTime} title={'Snoozed'} number={54}
+            />
+            <SidebarOption
+                Icon={LabelImportant} title={'Important'} number={54}
+            />
+            <SidebarOption
+                Icon={NearMe} title={'Sent'} number={54}
+            />
+            <SidebarOption
+                Icon={Note} title={'Drafts'} number={54}
+            />
+            <SidebarOption
+                Icon={ExpandMore} title={'More'} number={54}
+            />
+
+            <div className="sidebar__footer">
+                <div className="sidebar__footerIcons">
+                    <IconButton>
+                        <Person/>
+                    </IconButton>
+                    <IconButton>
+                        <Duo/>
+                    </IconButton>
+                    <IconButton>
+                        <Phone/>
+                    </IconButton>
+                </div>
+            </div>
         </div>
     );
 }
